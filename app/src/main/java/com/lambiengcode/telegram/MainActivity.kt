098201a5a521cb882c99613854df2f12ce2425c6
Dao Hong Vinh <60530946+lambiengcode@users.noreply.github.com>
2021-03-10 20:40:00 +0700

    initial commit

---
+++ app/src/main/java/com/lambiengcode/telegram/MainActivity.kt
@@ -0,0 +1,129 @@
+package com.lambiengcode.telegram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+
+import com.lambiengcode.telegram.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: Toolbar
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields()
+        initFunc()
+    }
+
+    private fun initFunc() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withActionBarDrawerToggle(true)
+                .withSelectedItem(-1)
+                .withAccountHeader(mHeader)
+                .addDrawerItems(
+                        PrimaryDrawerItem().withIdentifier(100)
+                                .withIconTintingEnabled(true)
+                                .withName("Создать группу")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_create_groups),
+                        PrimaryDrawerItem().withIdentifier(101)
+                                .withIconTintingEnabled(true)
+                                .withName("Создать секретный чат")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_secret_chat),
+                        PrimaryDrawerItem().withIdentifier(102)
+                                .withIconTintingEnabled(true)
+                                .withName("Создать канал")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_create_channel),
+                        PrimaryDrawerItem().withIdentifier(103)
+                                .withIconTintingEnabled(true)
+                                .withName("Контакты")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_contacts),
+                        PrimaryDrawerItem().withIdentifier(104)
+                                .withIconTintingEnabled(true)
+                                .withName("Звонки")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_phone),
+                        PrimaryDrawerItem().withIdentifier(105)
+                                .withIconTintingEnabled(true)
+                                .withName("Избранное")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_favorites),
+                        PrimaryDrawerItem().withIdentifier(106)
+                                .withIconTintingEnabled(true)
+                                .withName("Настройки")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_settings),
+                        PrimaryDrawerItem().withIdentifier(107)
+                                .withIconTintingEnabled(true)
+                                .withName("Создать группу")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_create_groups),
+                        DividerDrawerItem(),
+                        PrimaryDrawerItem().withIdentifier(108)
+                                .withIconTintingEnabled(true)
+                                .withName("Пригласить друзей")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_invate),
+                        PrimaryDrawerItem().withIdentifier(109)
+                                .withIconTintingEnabled(true)
+                                .withName("Вопросы о телеграм")
+                                .withSelectable(false)
+                                .withIcon(R.drawable.ic_menu_help)
+                ).withOnDrawerItemClickListener(object :Drawer.OnDrawerItemClickListener{
+                    override fun onItemClick(
+                            view: View?,
+                            position: Int,
+                            drawerItem: IDrawerItem<*>
+                    ): Boolean {
+                        Toast.makeText(applicationContext,position.toString(),Toast.LENGTH_SHORT).show()
+                        return false
+                    }
+                }).build()
+
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        ProfileDrawerItem().withName("lambiengcode")
+                                .withEmail("+7911111111")
+                ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
+    }
+}
